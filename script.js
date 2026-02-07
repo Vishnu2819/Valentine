79e1ca0ef567bb54e89cc8dd1fb5236a206f3326
your name <vishnuvulli20@gmail.com>
2026-02-07 16:54:40 -0500

    added tasks and few changes to the code

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const noBtn = document.querySelector('.no');
 const yesBtn = document.querySelector('.yes');
+const header = document.querySelector('h1');
 
 let scale = 1;
 
 noBtn.addEventListener('mouseenter', () => {
-    const maxX = window.innerWidth - noBtn.offsetWidth ;
-    const maxY = window.innerHeight - noBtn.offsetHeight ;
+    const rect = noBtn.getBoundingClientRect();
 
-    const x = Math.random() * maxX ;
-    const y = Math.random() * maxY ;
+    const maxX = window.innerWidth - rect.width;
+    const maxY = window.innerHeight - rect.height;
+
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
 
     noBtn.style.left = `${x}px`;
     noBtn.style.top = `${y}px`;
 
     scale = Math.min(scale + 0.15, 5);
     yesBtn.style.transform = `scale(${scale})`;
+
+    header.innerHTML = "You will be my Valentine!!";
 });
