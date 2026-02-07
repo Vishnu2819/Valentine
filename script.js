cde328647932a5f3e16d03a8f65408ac2b381903
your name <vishnuvulli20@gmail.com>
2026-02-07 13:25:54 -0500

    project initialised

---
+++ script.js
@@ -0,0 +1,18 @@
+const noBtn = document.querySelector('.no');
+const yesBtn = document.querySelector('.yes');
+
+let scale = 1;
+
+noBtn.addEventListener('mouseenter', () => {
+    const maxX = window.innerWidth - noBtn.offsetWidth ;
+    const maxY = window.innerHeight - noBtn.offsetHeight ;
+
+    const x = Math.random() * maxX ;
+    const y = Math.random() * maxY ;
+
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+
+    scale = Math.min(scale + 0.15, 3);
+    yesBtn.style.transform = `scale(${scale}) rotate(${scale * 2}deg)`;
+});
